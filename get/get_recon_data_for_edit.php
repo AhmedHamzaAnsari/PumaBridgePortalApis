12c34ffd2c5e041b4d7045c25411faeb28f89e9e
owais gul <owaiskk366@gmail.com>
2025-05-02 03:03:37 -0700

    Partialaly merged faisal work

--- get/get_recon_data_for_edit.php
+++ get/get_recon_data_for_edit.php
@@ -8,14 +8,14 @@ $access_key = '03201232927';
 $pass = $_GET["key"];
 if ($pass != '') {
     $dealer_id = $_GET["dealer_id"];
+    $task_id = $_GET["task_id"];
+    $product_id = $_GET["product_id"];
+
     if ($pass == $access_key) {
-        $sql_query1 = "SELECT dz.*,dp.name as product_name,ds.name as dispenser_name,dl.lorry_no as tank_name,
-        (SELECT new_reading FROM dealer_reconcilation where nozle_id=dz.id order by id desc limit 1) as new_reading 
-        FROM dealers_nozzel dz 
-        join dealers_products as dp on dp.id=dz.products
-        join dealers_dispenser as ds on ds.id=dz.dispenser_id
-        join dealers_lorries as dl on dl.id=dz.tank_id 
-        where dz.dealer_id=$dealer_id order by dz.id;";
+        $sql_query1 = "SELECT rr.*,ap.name as product_name,dc.name as dealer_name FROM dealer_stock_recon_new as rr
+        join dealers_products as dp on dp.id=rr.product_id
+       join all_products as ap on ap.name=dp.name
+       join dealers as dc on dc.id=rr.dealer_id where rr.task_id=$task_id and rr.dealer_id=$dealer_id and rr.product_id=$product_id group by rr.product_id;";
 
         $result1 = $db->query($sql_query1) or die("Error :" . mysqli_error($db));
 
