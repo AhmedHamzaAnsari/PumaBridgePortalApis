32e1a1e33ecb22bdf213713ee57f3a4600b9dbe4
Owais Gul <owaiskk366@gmail.com>
2025-05-09 18:12:23 +0500

    modified the path for uplaods and files.

--- config.php
+++ config.php
@@ -12,7 +12,7 @@ header('Access-Control-Allow-Headers: Content-Type');
 
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'Ptoptrack@(!!@');
+define('DB_PASSWORD', '');
 define('DB_DATABASE', 'pumasalesbridge');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
